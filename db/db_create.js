ee0d5e581b48716471a3cf00e785aac5616c98a1
Kay Hwang <khwang0529@gmail.com>
2024-03-06 23:33:58 -0500

    updated sql

--- db/db_create.js
+++ db/db_create.js
@@ -7,12 +7,15 @@ db.execute (drop_content_table_sql);
 const drop_comment_table_sql = "drop table if exists comment";
 db.execute (drop_comment_table_sql);
 
+const set_foriegn_key_checks = `SET foreign_key_checks = 0`;
+db.execute (set_foriegn_key_checks);
 
 //Create the subject table
 const create_content_table_sql = `CREATE TABLE content (
     contentId INT NOT NULL AUTO_INCREMENT,
     description VARCHAR(100) NOT NULL,
-    PRIMARY KEY (contentId));`;
+    PRIMARY KEY (contentId))
+    ENGINE = InnoDB;`;
 
 db.execute(create_content_table_sql)
 
@@ -21,23 +24,29 @@ db.execute(create_content_table_sql)
 const create_comment_table_sql = `CREATE TABLE comment(
     commentID INT NOT NULL AUTO_INCREMENT,
     text VARCHAR(100) NOT NULL,
+    KEY (comment_contentID),
     PRIMARY KEY (commentID),
+    comment_contentID VARCHAR (100) NOT NULL,
     CONSTRAINT fk_contentComment
       FOREIGN KEY (comment_contentID) 
       REFERENCES content (contentID)
       ON DELETE RESTRICT
-      ON UPDATE CASCADE);`;
+      ON UPDATE CASCADE)
+    ENGINE = InnoDB;`;
 db.execute(create_comment_table_sql)
 
 const create_reply_table_sql = `CREATE TABLE reply(
     replyID INT NOT NULL AUTO_INCREMENT,
     replies VARCHAR(100) NOT NULL,
     PRIMARY KEY (replyID),
+    reply_commentID VARCHAR (100) NOT NULL,
+    KEY (reply_commentID),
     CONSTRAINT commentReply
       FOREIGN KEY (reply_commentID) 
       REFERENCES comment (commentID)
       ON DELETE RESTRICT
-      ON UPDATE CASCADE);`;
+      ON UPDATE CASCADE)
+    ENGINE = InnoDB;`;
 db.execute(create_reply_table_sql)
 
 
